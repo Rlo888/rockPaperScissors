11c1b55fe0148a1b448c4591cf3fb67a545a69d5
Ryan <asdfasdf>
2022-10-11 23:18:39 +0800

    Completed the playARound function to return a str

--- main.js
+++ main.js
@@ -1,16 +1,57 @@
 function getComputerChoice(arr) {
+  arr = choice;
   const randomIndex = Math.floor(Math.random() * arr.length);
+  let result = "";
   if (randomIndex === 0) {
-    return "rock";
+    result = "rock";
   }
   if (randomIndex === 1) {
-    return "paper";
+    result = "paper";
   } else {
-    return "scissors";
+    result = "scissors";
   }
+  return result;
 }
 
 const choice = ["rock", "paper", "scissors"];
 const result = getComputerChoice(choice);
 
-console.log(getComputerChoice(choice));
+//console.log(getComputerChoice(choice));
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "rock" && computerSelection == "rock") {
+    return "Go Again!";
+  }
+  if (playerSelection == "rock" && computerSelection == "paper") {
+    return "Paper Beats Rock!";
+  }
+
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "Rock Beats Scissors!";
+  }
+  if (playerSelection == "paper" && computerSelection == "rock") {
+    return "Paper Beats Rock!";
+  }
+  if (playerSelection == "paper" && computerSelection == "paper") {
+    return "Go Again!";
+  }
+  if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "Scissors Beats Paper!";
+  }
+  if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "Scissors Beats Rock!";
+  }
+  if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "Scissors Beats Paper!";
+  }
+  if (playerSelection == "scissors" && computerSelection == "scissors") {
+    return "Go Again!";
+  } else {
+    return "Oh noes!";
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+// console.log(computerSelection);
